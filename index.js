7e2ac9642acd9e9d30e2605e7cfbb45a8fa3cfdb
krishna.ac18 <krishna.acharya@rewasoft.com.np>
2025-02-28 23:09:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/verify', (req, res) => {
+  const { code } = req.body;
+
+  if (!code || code.length !== 6 || isNaN(code) || code[5] === '7') {
+    return res.status(400).json({
+      error: 'Invalid code.'
+    });
+  }
+
+  res.json({
+    message: `Code verified`
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
